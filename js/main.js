d391fb40c65d9bd1a90b990d1b5db6febfb31343
RitaStark <ritastar13@gmail.com>
2019-09-22 20:59:10 +0300

    beginning

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+for (var i = 0; i <= COUNT - 1; i++) {
+  var COUNT = 25;
+  var objectItem = {
+    url: '',
+    description: '',
+    likes: 10,
+    comments = {
+      avatar: ,
+      message: ,
+      name: ,
+    },
+  }
+  massiveOfObjects[i] = objectItem;
+  console.log(objectItem);
+};
+
+
