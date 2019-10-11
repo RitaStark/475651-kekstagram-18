59665f5b9612fe950a07eec2b408fcc01c81ccaf
RitaStark <ritastar13@gmail.com>
2019-10-11 20:44:08 +0300

    show popup + scale

--- js/main.js
+++ js/main.js
@@ -69,3 +69,59 @@ fillBlock(picture);
 pictureInfo.appendChild(fragment);
 
 
+// --------------------------------------------------------------------------------
+
+
+var upload = document.querySelector('#upload-file');
+var changeImage = document.querySelector('.img-upload__overlay');
+var closeButton = changeImage.querySelector('.img-upload__cancel');
+
+upload.addEventListener('change', function (evt) {
+  evt.preventDefault();
+  changeImage.classList.remove('hidden');
+});
+
+closeButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  changeImage.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (evt) {
+  console.log('keydown');
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    changeImage.classList.add('hidden');
+  }
+});
+
+
+
+var scaleControl = document.querySelector(".scale");
+var scaleSmaller = scaleControl.querySelector(".scale__control--smaller");
+var scaleBigger = scaleControl.querySelector(".scale__control--bigger");
+var scaleValue = scaleControl.querySelector(".scale__control--value");
+
+scaleValue.value = 100;
+var numberScaleValue = Number(scaleValue.value);
+
+var scaleSmallerClickHandler = function () {
+  if (numberScaleValue > 25 && numberScaleValue <= 100) {
+    numberScaleValue -= 25;
+    var percentScaleValue = numberScaleValue + "%";
+    console.log(percentScaleValue);
+  }
+};
+scaleSmaller.addEventListener("click", scaleSmallerClickHandler);
+
+
+var scaleBiggerClickHandler = function () {
+  if (numberScaleValue >= 25 && numberScaleValue < 100) {
+    numberScaleValue += 25;
+    var percentScaleValue = numberScaleValue + "%";
+    console.log(percentScaleValue);
+  }
+};
+scaleBigger.addEventListener("click", scaleBiggerClickHandler);
+
+
+
