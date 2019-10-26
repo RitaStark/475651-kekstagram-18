431cfd17ec192ea0fa18087a981f2d85f8bf1737
RitaStark <ritastar13@gmail.com>
2019-10-26 15:45:34 +0300

    tag validation

--- js/main.js
+++ js/main.js
@@ -276,21 +276,52 @@ var userTagInput = document.querySelector("input.text__hashtags");
 // });
 
 var userTagInput = document.querySelector("input.text__hashtags");
-var tagsList = "t, tag2, tag3, tag4, tag5";
-var tagsArr = tagsList.split(", ");
+
+var arrContains = function (elem, arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (elem.toLowerCase() == arr[i].toLowerCase()) {
+      return true;
+    }
+  }
+  return false;
+};
+
 userPhotoTag.addEventListener("input", function () {
+  var tagsList = userTagInput.value;
+  var tagsArr = tagsList.split(" ");
+  console.log("tagsArr", tagsArr);
+
   for (var i = 0; i < tagsArr.length; i++) {
     var tag = tagsArr[i];
+    var validationError = "";
     if (tag.length < 2) {
-      userTagInput.setCustomValidity("Хэш-тег должен состоять минимум из 2-х символов и начинаться с \#");
+      // userTagInput.setCustomValidity("Хэш-тег должен состоять минимум из 2-х символов");
+      validationError = "Хэш-тег должен состоять минимум из 2-х символов";
     } else if (tag.length > 20) {
-      userTagInput.setCustomValidity("Хэш-тег должен состоять максимум из 20-ти символов");
-    } else {
-      userTagInput.setCustomValidity("");
-    };
-    // console.log("Тэг:" + tag);
+      validationError = "Хэш-тег должен состоять максимум из 20-ти символов";
+    } else if (!tag.startsWith("#")) {
+      validationError = "Хэш-тег должен начинаться с \#";
+    } else if (arrContains(tag, tagsArr.slice(0, i))) {
+      validationError = "Хэш-теги не должны повторяться";
+    }
+    userTagInput.setCustomValidity(validationError);
+    console.log("validating tag '" + tag + "' result: " + validationError);
+    if (validationError !== "") {
+      break;
+    }
   };
 });
+
+
+
+// userTagInput.onblur = function () {
+//   if (!userTagInput.value.includes("#")) {
+//     userTagInput.setCustomValidity("Хэш-тег должен начинаться с \#");
+//   }
+//   // } else if (tag.value == tag.value) {
+//   //   userTagInput.setCustomValidity("Хэш-теги не должены повторяться");
+//   // };
+// };
 
 
 
